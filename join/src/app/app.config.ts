9eadc36e9486c9c4cc2ce4d809812db229978058
koschkx@gmail.com <96351407+KoschKX@users.noreply.github.com>
2025-07-23 15:28:20 +0200

    update config

--- join/src/app/app.config.ts
+++ join/src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,18 +15,10 @@ export const appConfig: ApplicationConfig = {
       : provideRouter(routes, withHashLocation()),
     provideAnimations(), 
     provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideHttpClient(),
 
-    provideFirebaseApp(() => initializeApp({ 
-      projectId: "join-project-a437a", 
-      appId: "1:848614889298:web:c448e996f46de06252cc8b", 
-      databaseURL: "https://join-project-a437a-default-rtdb.europe-west1.firebasedatabase.app", 
-      storageBucket: "join-project-a437a.firebasestorage.app", 
-      apiKey: "AIzaSyAMajQkymTiHB35Z5gmSzy8ezCv432D87A", 
-      authDomain: "join-project-a437a.firebaseapp.com", 
-      messagingSenderId: "848614889298", 
-      measurementId: "G-X4R986D1CT" 
-    })), 
-
+    provideFirebaseApp(() => initializeApp(environment.firebase)), 
+    provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
   ]
 };
--- join/src/main.ts
+++ join/src/main.ts
@@ -1,22 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth } from '@angular/fire/auth';
-import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [
-    ...(appConfig.providers || []),
-    provideRouter(routes),
-    provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth())
-  ]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
